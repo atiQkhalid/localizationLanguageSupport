8af422e97a8ce1c988f6f49ccbca117402e24337
atiQ bin khalid <39992488+atiQkhalid@users.noreply.github.com>
2022-06-20 10:36:59 +0300

    localized different languages

--- app/src/main/java/com/example/localizationlanguagesupport/MainActivity.kt
+++ app/src/main/java/com/example/localizationlanguagesupport/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.localizationlanguagesupport
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.localizationlanguagesupport.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        loadLocate()
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        binding.tvEn.setOnClickListener {
+            setLocate("en")
+            recreate()
+        }
+        binding.tvTr.setOnClickListener {
+            setLocate("tr")
+            recreate()
+        }
+    }
+
+    private fun setLocate(Lang: String){
+        val locale = Locale(Lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
+    }
+
+    fun loadLocate(){
+        var preferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        var localizedLanguage = preferences.getString("My_Lang", "")
+        setLocate(localizedLanguage!!)
     }
 }
